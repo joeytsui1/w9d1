17440ee78a1b6f90fdc1e2d517dc0c69f3c99bcf
Joey Tsui <joeytsui131@gmail.com>
2023-01-09 12:19:32 -0500

    asteroids

--- asteroids/src/index.js
+++ asteroids/src/index.js
@@ -1,3 +1,4 @@
 const MovingObject = require("./moving_object.js");
 
+window.MovingObject = MovingObject;
 console.log("Webpack is working")
--- asteroids/src/moving_object.js
+++ asteroids/src/moving_object.js
@@ -1,17 +1,23 @@
 
-function MovingObject (obj){
-    this.pos = obj[pos]; 
-    this.vel = obj[vel];
-    this.radius = obj[radius];
-    this.color = obj[color];
+function MovingObject (pos, vel, radius, color){
+    this.pos = pos; 
+    this.vel = vel;
+    this.radius = radius;
+    this.color = color;
 }
 
+// function MovingObject(obj) {
+//     this.pos = obj[pos];
+//     this.vel = obj[vel];
+//     this.radius = obj[radius];
+//     this.color = obj[color];
+// }
 
-// const mo = new MovingObject({
-//     pos: [30, 30],
-//     vel: [10, 10],
-//     radius: 5,
-//     color: "#00FF00"
-// });
+const mo = new MovingObject({
+    pos: [30, 30],
+    vel: [10, 10],
+    radius: 5,
+    color: "#00FF00"
+});
 
 module.exports = MovingObject;
